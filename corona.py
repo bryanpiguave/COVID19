b9edb22f3a1ce487d7d339120d017971dd10d42c
bryanpiguave <bpiguave@espol.edu.ec>
2020-03-27 15:54:01 -0500

    El archivo carga el csv en dataframe y headers obtiene una lista de que se significan los valores de cada columna

---
+++ corona.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 10:59:08 2020
+
+@author: Bryan Piguave
+"""
+
+import pandas as pd
+path=r'C:\Users\usuario\Dropbox\PythonEnhanced\COVID19\corona.csv'
+df = pd.read_csv(path,sep=';')
+
+Headers =df.columns
+
+
